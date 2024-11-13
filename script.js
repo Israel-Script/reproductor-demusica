9583cda23ae30d6e612c90c3015def581df934dc
israel <learsitoromero@gmail.com>
2024-11-13 16:15:39 -0400

    pagina finalizada

--- script.js
+++ script.js
@@ -4,13 +4,34 @@ const crearcancion = (songdata) => {
     const div = document.createElement ('div')
     div.setAttribute("class","song")
     div.innerHTML = `
+    <div class="container">
     <img src="${"https://api.institutoalfa.org/api/songs/image/" + songdata.image.filename}"alt="">
+    </div>
     <p>${songdata.title}</p>
-    `
 
+
+    `
+    div.addEventListener("click", () => {
+        document.getElementById("audio")
+        .setAttribute('src', "https://api.institutoalfa.org/api/songs/audio/" + songdata.audio.filename)
+    })
+
+    
+    
     return div
 } 
- 
+
+document.getElementById("play").addEventListener("click" , () => {
+    const audio = document.getElementById("audio")
+
+    console.log(audio)
+
+    if (audio.paused) {
+        audio.play()
+    } else{
+        audio.pause()
+    }
+})
 
 console.log (axios)
 
